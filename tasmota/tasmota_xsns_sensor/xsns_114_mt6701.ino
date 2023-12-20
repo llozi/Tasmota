952b8bcbbae28a8b19c08458d625c4834213e122
Lukas <llo@bluewin.ch>
2023-12-20 16:49:45 +0100

    Started to implement xsns_114_mt6701.ino.

--- tasmota/tasmota_xsns_sensor/xsns_114_mt6701.ino
+++ tasmota/tasmota_xsns_sensor/xsns_114_mt6701.ino
@@ -31,10 +31,94 @@
  *
 \*********************************************************************************************/
 
-#define XSNS_100                                100
+#define XSNS_114                                114
 #define XI2C_86                                 86        // See I2CDEVICES.md
 
 #define MT6701_ADDRESS                          (0x06)    // 0000110
+
+uint8_t mt6701_found = 0;
+
+bool mt6701Init(void)
+{
+  Wire.beginTransmission(MT6701_ADDRESS);
+  Wire.endTransmission();
+  delay(1);
+
+  Wire.beginTransmission(MT6701_ADDRESS); // start transmission
+  if (Wire.endTransmission(1) != 0) {
+    return false;
+  }
+  return true;
+}
+
+void mt6701Detect(void)
+{
+  if (mt6701Init()) {
+    if (!mt6701_found) {
+      AddLog(LOG_LEVEL_INFO, S_LOG_I2C_FOUND_AT, AM2320_types, MT6701_ADDRESS);
+    } else {
+      AddLog(LOG_LEVEL_DEBUG, S_LOG_I2C_FOUND_AT, AM2320_types, MT6701_ADDRESS);
+    }
+    mt6701_found = 3;
+  } else {
+    if (mt6701_found) { mt6701_found--; }
+  }
+}
+
+
+void mt6701EverySecond(void)
+{
+}
+
+/*********************************************************************************************\
+ * Interface
+\*********************************************************************************************/
+
+/**
+ * The callback function Xsns114() interfaces Tasmota with the MT6701 sensor driver.
+ *
+ * It provides the Tasmota callback IDs.
+ *
+ * @param   byte    callback_id  Tasmota function ID.
+ * @return  boolean              Return value.
+ * @pre     None.
+ * @post    None.
+ *
+ */
+bool Xsns114(byte callback_id) {
+
+  // Set return value to `false`
+  bool result = false;
+
+  // Check if there is another I2C driver enabled with the same address range id
+  if (!I2cEnabled(XI2C_86)) { return false; }
+
+  // Check which callback ID is called by Tasmota
+  if (callback_id == FUNC_INIT) {
+    mt6701Detect();
+  } else if (mt6701_found) {
+    switch (callback_id) {
+      case FUNC_EVERY_50_MSECOND:
+        break;
+      case FUNC_EVERY_SECOND:
+        mt6701EverySecond();
+        break;
+      case FUNC_JSON_APPEND:
+        break;
+#ifdef USE_WEBSERVER
+      case FUNC_WEB_APPEND:
+        break;
+#endif // USE_WEBSERVER
+      case FUNC_SAVE_BEFORE_RESTART:
+        break;
+      case FUNC_COMMAND:
+        break;
+    }
+  }
+
+  // Return success in case of successful command callback
+  return result;
+}
 
 #endif  // USE_MT6701
 #endif  // USE_I2C
